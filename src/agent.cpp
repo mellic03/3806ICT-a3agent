1245a911f4966900b8bc4f65c936a72c20b4d224
mellic03 <mellic03@gmail.com>
2024-05-29 21:17:39 +1000

    Print a3planner plan

--- src/agent.cpp
+++ src/agent.cpp
@@ -3,9 +3,109 @@
 
 
 void
+Agent::scanning_behaviour()
+{
+    static float total = 0.0f;
+
+    // if (m_bearing >= 2.0f*M_PI)
+    // {
+    //     m_bearing = 0.0f;
+    //     m_state = STATE_IDLE;
+    // }
+
+    // else
+    // {
+    //     m_linear  = 0.0f;
+    //     m_bearing = 0.1f;
+    // }
+}
+
+
+void
+Agent::idle_behaviour()
+{
+    if (!m_plan_client->call(m_plan_srv))
+    {
+        ROS_ERROR("Could not call service \"/a3planner/plan\"");
+        return;
+    }
+
+    m_state = STATE_FOLLOWING_PLAN;
+
+
+    for (int i=0; i<10; i++)
+    {
+        std::cout << char(m_plan_srv.response.plan[i]) << " ";
+    }
+    std::cout << "\n";
+
+    // if (m_sonar_dist < 1.0f)
+    // {
+    //     m_linear = 0.0f;
+
+    //     float prev = m_bearing;
+    //     m_bearing += 0.01f;
+    //     // m_bearing = (rand() % 100) / 100.0f;
+    //     // m_bearing -= 0.5f;
+    //     // m_bearing *= 3.14159f;
+    //     // m_bearing = 0.8f*prev + 0.2f*m_bearing;
+    // }
+
+    // else
+    // {
+    //     m_linear  = 0.1f;
+
+    //     float prev = m_bearing;
+    //     m_bearing -= 0.01f;
+    //     // m_bearing = (rand() % 100) / 100.0f;
+    //     // m_bearing -= 0.5f;
+    //     // m_bearing *= 3.14159f;
+    //     // m_bearing = 0.8f*prev + 0.2f*m_bearing;
+    // }
+}
+
+
+void
+Agent::follow_behaviour()
+{
+    static int current = 0;
+
+    auto &plan = m_plan_srv.response.plan;
+
+    if (plan[current] != 0)
+    {
+
+    }
+
+}
+
+
+
+void
+Agent::update()
+{
+    switch (m_state)
+    {
+        case STATE_IDLE:            idle_behaviour();     break;
+        case STATE_SCANNING:        scanning_behaviour(); break;
+        case STATE_FOLLOWING_PLAN:  follow_behaviour();   break;
+    }
+
+    update_motors();
+}
+
+
+
+void
 Agent::sonars_callback( const a3env::sonars &msg )
 {
+    // static uint64_t prev_utc = msg.timestamp;
+    // uint64_t utc = msg.timestamp;
     m_sonar_dist = msg.distance;
+    // float delta = (float(utc) / float(prev_utc)) / 1000000.0f;
+    // std::cout << "dt: " << delta << "\n";
+    // prev_utc = utc;
+
 
     glm::vec2 dir = glm::vec2(cos(m_bearing), sin(m_bearing));
     glm::vec2 cell = m_position + m_sonar_dist*dir;
@@ -40,8 +140,7 @@ Agent::sonars_callback( const a3env::sonars &msg )
         m_plan_srv.request.world[W*row + col] = uint8_t(msg.blocktype);
     }
 
-
-    update_motors();
+    update();
 }
 
 
@@ -50,43 +149,19 @@ Agent::odom_callback( const a3env::odom &msg )
 {
     m_position.x = msg.xpos;
     m_position.y = msg.ypos;
-    m_bearing    = msg.bearing;
-    update_motors();
+    // m_bearing    = msg.bearing;
+
+    update();
 }
 
 
 void
 Agent::update_motors()
 {
-    // Currently just random movement.
-
-    if (m_sonar_dist < 1.0f)
-    {
-        m_linear = 0.0f;
-
-        float prev = m_angular;
-        m_angular = (rand() % 100) / 100.0f;
-        m_angular -= 0.5f;
-        m_angular *= 0.5f;
-        m_angular = 0.8f*prev + 0.2f*m_angular;
-    }
-
-    else
-    {
-        m_linear  = 0.1f;
-
-        float prev = m_angular;
-        m_angular = (rand() % 100) / 100.0f;
-        m_angular -= 0.5f;
-        m_angular *= 0.25f;
-        m_angular = 0.8f*prev + 0.2f*m_angular;
-    }
-
-
     a3env::motors srv;
     srv.request.agentid = m_ID;
     srv.request.linear  = m_linear;
-    srv.request.angular = m_angular;
+    srv.request.bearing = m_bearing;
 
 
     if (!m_motors_client->call(srv))
@@ -112,4 +187,48 @@ Agent::request_plan()
     }
 
 }
+
+
+
+void
+Agent::follow_plan()
+{
+    // uint16_t *plan = m_plan_srv.response.plan;
+
+    // static int current = 0;
+
+    // if (m_state != STATE_FOLLOWING_PLAN)
+    // {
+    //     m_state = STATE_FOLLOWING_PLAN;
+    //     current = 0;
+    // }
+
+    // if (plan[current] != 0)
+    // {
+    //     uint16_t action = plan[current];
+
+    //     switch (action)
+    //     {
+    //         case 'L':   break;
+    //         case 'R':   break;
+    //         case 'U':   break;
+    //         case 'D':   break;
+
+    //         default:
+    //             break;
+    //     }
+
+    //     current += 1;
+    // }
+
+    // else
+    // {
+    //     m_state = STATE_IDLE;
+    // }
+
+
+
+
+}
+
 
--- src/agent.hpp
+++ src/agent.hpp
@@ -19,12 +19,14 @@ private:
     enum State
     {
         STATE_IDLE,
+        STATE_SCANNING,
+        STATE_FOLLOWING_PLAN,
         STATE_MOVING_TO_SURVIVOR,
         STATE_RETURNING_HOME,
         STATE_SOMETHING_ELSE
     };
 
-    Agent::State m_state = STATE_IDLE;
+    Agent::State m_state;
 
 
 public:
@@ -35,7 +37,7 @@ public:
     a3planner::plan      m_plan_srv;
 
     glm::vec2 m_position;
-    float     m_bearing;
+    float     m_bearing = 0.0f;
     float     m_linear;
     float     m_angular;
     float     m_sonar_dist;
@@ -45,8 +47,11 @@ public:
     Agent( int id, ros::ServiceClient *plan_client, ros::ServiceClient *motors_client )
     :   m_ID            (id),
         m_plan_client   (plan_client),
-        m_motors_client (motors_client)
+        m_motors_client (motors_client),
+        m_state         (STATE_IDLE)
     {
+        m_plan_srv.request.world.resize(a3env::MAP_WIDTH*a3env::MAP_WIDTH);
+
         for (int i=0; i<a3env::NUM_HOSTILES; i++)
         {
             m_plan_srv.request.hostiles[i] = -1;
@@ -56,9 +61,16 @@ public:
     void sonars_callback( const a3env::sonars &msg );
     void odom_callback( const a3env::odom &msg );
 
-    void request_plan();
     void update_motors();
 
+    void request_plan();
+    void follow_plan();
+
+    void update();
+
+    void idle_behaviour();
+    void scanning_behaviour();
+    void follow_behaviour();
 };
 
 
